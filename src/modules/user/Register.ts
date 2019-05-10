d06462a712c3090bafee7b9526751048bf5ff971
Pieter Overbeek <pieter@belfabriek.nl>
2019-05-10 12:01:55 +0300

    Setup

---
+++ src/modules/user/Register.ts
@@ -0,0 +1,30 @@
+import { User } from './../../entity/User'
+import { Resolver, Query, Mutation, Arg } from 'type-graphql'
+import bcrypt from 'bcryptjs'
+
+@Resolver(User)
+export class RegisterResolver {
+  @Query(() => String, { name: 'helloWorld' })
+  async hello() {
+    return 'Hello World!'
+  }
+
+  @Mutation(() => User)
+  async register(
+    @Arg('firstName') firstName: string,
+    @Arg('lastName') lastName: string,
+    @Arg('email') email: string,
+    @Arg('password') password: string,
+  ): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 12)
+
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    }).save()
+
+    return user
+  }
+}
